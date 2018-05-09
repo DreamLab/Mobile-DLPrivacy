6f85a2dabe3ba3b312f29e0cf9df8f638b5af20f
Szeremeta Adam <Adam.Szeremeta@dreamlab.pl>
2018-05-09 15:25:26 +0200

    Something

---
+++ Pod/Classes/Views/DLPrivacyFormView.swift
@@ -0,0 +1,15 @@
+//
+//  DLPrivacyFormView.swift
+//  DLPrivacy
+//
+//  Created by Szeremeta Adam on 09.05.2018.
+//  Copyright © 2018 DreamLab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class DLPrivacyFormView: UIView {
+
+
+}
